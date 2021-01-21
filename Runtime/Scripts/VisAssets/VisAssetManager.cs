3e778a2ef9cfa4a3a8cd597513696be45e51f6f0
Bridger Herman <herma582@umn.edu>
2021-01-20 19:01:30 -0600

    Add glyph support and OBJImport dependency

--- Runtime/Scripts/VisAssets/VisAssetManager.cs
+++ Runtime/Scripts/VisAssets/VisAssetManager.cs
@@ -12,7 +12,8 @@ using System.Collections.Generic;
 
 using Newtonsoft.Json.Linq;
 
-namespace IVLab.ABREngine {
+namespace IVLab.ABREngine
+{
     public class VisAssetManager : IVLab.Utilities.Singleton<VisAssetManager>
     {
         public string appDataPath;
@@ -21,20 +22,37 @@ namespace IVLab.ABREngine {
 
         private Dictionary<Guid, IVisAsset> _visAssets = new Dictionary<Guid, IVisAsset>();
 
-        void Start() {
+        void Start()
+        {
             appDataPath = Path.Combine(Application.persistentDataPath, "media", "visassets");
             Directory.CreateDirectory(appDataPath);
             LoadVisAssetPalette();
         }
 
+        string VisAssetDataPath(string artifactFilePath, string relativeDataPath)
+        {
+            return Path.Combine(Path.GetDirectoryName(artifactFilePath), relativeDataPath);
+        }
+
         public void LoadVisAssetPalette()
         {
             string[] files = Directory.GetFiles(appDataPath, VISASSET_JSON, SearchOption.AllDirectories);
+            Debug.LogFormat("Loading VisAsset Palette ({0} VisAssets)", files.Length);
 
+            int success = 0;
             foreach (var filePath in files)
             {
-                LoadVisAsset(filePath);
+                try
+                {
+                    LoadVisAsset(filePath);
+                    success += 1;
+                }
+                catch (Exception e)
+                {
+                    Debug.LogErrorFormat("Error loading VisAsset {0}:\n{1}", filePath, e);
+                }
             }
+            Debug.LogFormat("Successfully loaded {0}/{1} VisAssets", success, files.Length);
         }
 
         public void LoadVisAsset(Guid visAssetUUID)
@@ -47,7 +65,7 @@ namespace IVLab.ABREngine {
             LoadVisAsset(visassetPath);
         }
 
-        public void LoadVisAsset(string filePath, bool replaceExisting=false)
+        public void LoadVisAsset(string filePath, bool replaceExisting = false)
         {
             StreamReader reader = new StreamReader(filePath);
             JObject jsonData = JObject.Parse(reader.ReadToEnd());
@@ -66,169 +84,158 @@ namespace IVLab.ABREngine {
                 Debug.LogWarning(string.Format("VisAsset {0}: Use of field `type` is deprecated. Use `artifactType` instead.", guid.ToString().Substring(0, 8)));
             }
 
-            if (type == "colormap")
+            try
             {
-                ColormapVisAsset visAsset;
-                if (!replaceExisting)
+                if (type == "colormap")
                 {
-                    visAsset = new ColormapVisAsset();
-                    visAsset.Uuid = guid;
-                }
-                else
-                {
-                    visAsset = _visAssets[guid] as ColormapVisAsset;
-                }
-                visAsset.ImportTime = DateTime.Now;
+                    ColormapVisAsset visAsset;
+                    if (replaceExisting && _visAssets.ContainsKey(guid))
+                    {
+                        visAsset = _visAssets[guid] as ColormapVisAsset;
+                    }
+                    else
+                    {
+                        visAsset = new ColormapVisAsset();
+                        visAsset.Uuid = guid;
+                    }
+                    visAsset.ImportTime = DateTime.Now;
 
-                try
-                {
-                    string relativeColormapPath = "colormap.xml";//jsonData["artifactData"]["image"].str;
+                    string relativeColormapPath = jsonData["artifactData"]["colormap"].ToString();
 
                     string colormapfilePath = Path.Combine(Path.GetDirectoryName(filePath), relativeColormapPath);
                     Texture2D texture = null;
 
-                    if (File.Exists(colormapfilePath)) // ERROR: The name 'File' does not exist in the current context?
+                    if (File.Exists(colormapfilePath))
                     {
                         texture = ColormapUtilities.ColormapFromFile(colormapfilePath);
                         visAsset.Gradient = texture;
                     }
-                }
-                catch (System.Exception e) { 
-                    Debug.LogError("VisAsset not suppported yet: " + type + " (" + jsonData["uuid"].ToString() + ")");
-                    Debug.LogError(e);
+
+                    _visAssets[guid] = visAsset;
                 }
 
+                if (type == "glyph")
+                {
+                    GlyphVisAsset visAsset = new GlyphVisAsset();
+                    visAsset.Uuid = guid;
+                    visAsset.ImportTime = DateTime.Now;
+
+                    var artifactData = jsonData["artifactData"];
+                    List<JObject> lodsList = null;
+
+                    try
+                    {
+                        lodsList = artifactData["lods"].ToObject<List<JObject>>();
+                    }
+                    catch (ArgumentException)
+                    {
+                        if (artifactData is JArray)
+                        {
+                            Debug.LogWarning(string.Format(
+                                "VisAsset {0}: Use of bare array in `artifactData` is deprecated. Put the array inside an object.",
+                                guid.ToString().Substring(0, 8)
+                            ));
+                            lodsList = artifactData.ToObject<List<JObject>>();
+                        }
+                    }
+                    foreach (JObject lodJson in lodsList)
+                    {
+                        var meshPath = VisAssetDataPath(filePath, lodJson["mesh"].ToString());
+                        var normalPath = VisAssetDataPath(filePath, lodJson["normal"].ToString());
+                        GameObject loadedObjGameObject = null;
+                        try
+                        {
+                            loadedObjGameObject = new IVLab.OBJImport.OBJLoader().Load(meshPath);
+                        }
+                        catch (System.Exception e)
+                        {
+                            Debug.LogError(e);
+                        }
+                        loadedObjGameObject.transform.SetParent(transform);
+                        loadedObjGameObject.SetActive(false);
+                        var loadedMesh = loadedObjGameObject.GetComponentInChildren<MeshFilter>().mesh;
+                        GameObject.Destroy(loadedObjGameObject);
+
+                        var normalData = File.ReadAllBytes(normalPath);
+                        var normalMap = new Texture2D(2, 2);
+                        normalMap.LoadImage(normalData);
+
+                        visAsset.MeshLods.Add(loadedMesh);
+                        visAsset.NormalMapLods.Add(normalMap);
+                    }
+
+                    _visAssets[guid] = visAsset;
+                }
+
+                //     if (type == "line")
+                //     {
+                //         LineTextureVisAsset visAsset = ABRManager.CreateNode<LineTextureVisAsset>(new System.Guid(jsonData["uuid"].ToString()));
+                //         visAsset.ImportTime = DateTime.Now;
+
+                //         ABRManager.Instance.SetNodeLabel(visAsset, jsonData["name"]?.ToString() ?? "");
+
+                //         var artifactData = jsonData["artifactData"];
+                //         try
+                //         {
+                //             if (artifactData.ContainsKey("horizontal"))
+                //             {
+                //                 var texturePath = VisAssetDataPath(filePath, artifactData["horizontal"].ToString());
+
+                //                 var textureData = File.ReadAllBytes(texturePath); // ERROR: The name 'File' does not exist in the current context?
+                //                 var texture = new Texture2D(2, 2);
+                //                 texture.LoadImage(textureData);
+
+                //                 visAsset.TextureArray = new Texture2D[] { texture };
+                //             }
+                //             else
+                //             {
+                //                 throw (new System.Exception());
+                //             }
+                //         }
+                //         catch (System.Exception e) { Debug.Log("VisAsset not suppported yet: " + type + " (" + jsonData["uuid"].ToString() + ")"); }
+                //     }
+
+
+                //     if (type == "texture")
+                //     {
+                //         SurfaceTextureVisAsset visAsset = ABRManager.CreateNode<SurfaceTextureVisAsset>(new System.Guid(jsonData["uuid"].ToString()));
+                //         visAsset.ImportTime = DateTime.Now;
+
+                //         ABRManager.Instance.SetNodeLabel(visAsset, jsonData["name"]?.ToString() ?? "");
+
+                //         var artifactData = jsonData["artifactData"];
+                //         try
+                //         {
+                //             if (artifactData.ContainsKey("image"))
+                //             {
+                //                 var texturePath = VisAssetDataPath(filePath, artifactData["image"].ToString());
+
+                //                 var textureData = File.ReadAllBytes(texturePath); // ERROR: The name 'File' does not exist in the current context?
+                //                 var texture = new Texture2D(2, 2);
+                //                 texture.LoadImage(textureData);
+
+                //                 visAsset.TextureArray = new Texture2D[] { texture };
+
+
+                //                 var normalPath = VisAssetDataPath(filePath, artifactData["normalmap"].ToString());
+
+                //                 var normalData = File.ReadAllBytes(normalPath); // ERROR: The name 'File' does not exist in the current context?
+                //                 var normal = new Texture2D(2, 2);
+                //                 normal.LoadImage(normalData);
+
+                //                 visAsset.NormalMapArray = new Texture2D[] { normal };
+                //             }
+                //             else
+                //             {
+                //                 throw (new System.Exception());
+                //             }
+                //         }
+                //         catch (System.Exception e) { Debug.Log("VisAsset not suppported yet: " + jsonData["name"]?.ToString() ?? "Untitled" + " (" + jsonData["uuid"].ToString() + ")"); }
+                //     }
             }
-
-        //     if (type == "glyph")
-        //     {
-        //         GlyphVisAsset visAsset = new GlyphVisAsset();
-        //         visAsset.Uuid = guid;
-        //         visAsset.ImportTime = DateTime.Now;
-
-        //         var artifactData = jsonData["artifactData"];
-        //         try
-        //         {
-        //             JObject lodsList = null;
-
-        //             if (artifactData.ContainsKey("lods"))
-        //             {
-        //                 lodsList = artifactData.GetField("lods");
-
-        //             } else if(artifactData.IsArray)
-        //             {
-        //                 lodsList = artifactData;
-        //             }
-
-        //             if (lodsList != null)
-        //             {
-
-        //                 foreach (var lodJson in lodsList.list)
-        //                 {
-        //                     var meshPath = ArtifactDataPath(filePath, lodJson["mesh"].ToString());
-        //                     var normalPath = ArtifactDataPath(filePath, lodJson["normal"].ToString());
-        //                     Debug.Log("Loading mesh: " + meshPath);
-        //                     GameObject loadedObjGameObject = null;
-        //                     try
-        //                     {
-        //                         loadedObjGameObject = new OBJLoader().Load(meshPath);
-        //                     }
-        //                     catch (System.Exception e)
-        //                     {
-        //                         Debug.Log(e);
-        //                     }
-        //                     Debug.Log(loadedObjGameObject);
-        //                     loadedObjGameObject.transform.SetParent(transform);
-        //                     loadedObjGameObject.SetActive(false);
-        //                     var loadedMesh = loadedObjGameObject.GetComponentInChildren<MeshFilter>().mesh;
-        //                     Debug.Log(loadedMesh);
-        //                     GameObject.Destroy(loadedObjGameObject);
-
-        //                     var normalData = File.ReadAllBytes(normalPath); // ERROR: The name 'File' does not exist in the current context?
-        //                     var normalMap = new Texture2D(2, 2);
-        //                     normalMap.LoadImage(normalData);
-
-
-        //                     visAsset.MeshLods.Add(loadedMesh);
-        //                     visAsset.NormalMapLods.Add(normalMap);
-
-        //                 }
-        //             }
-        //             else
-        //             {
-        //                 throw (new System.Exception());
-        //             }
-
-        //             visAsset.ReceiveMessage(new ABRUpdateMessage { Type = ABRUpdateMessage.UpdateType.Unknown });
-
-        //         }
-        //         catch (System.Exception e) { Debug.Log("VisAsset not suppported yet: " + type + " (" + jsonData["uuid"].ToString() + ")"); }
-        //     }
-        //     if (type == "line")
-        //     {
-        //         LineTextureVisAsset visAsset = ABRManager.CreateNode<LineTextureVisAsset>(new System.Guid(jsonData["uuid"].ToString()));
-        //         visAsset.ImportTime = DateTime.Now;
-
-        //         ABRManager.Instance.SetNodeLabel(visAsset, jsonData["name"]?.ToString() ?? "");
-
-        //         var artifactData = jsonData["artifactData"];
-        //         try
-        //         {
-        //             if (artifactData.ContainsKey("horizontal"))
-        //             {
-        //                 var texturePath = ArtifactDataPath(filePath, artifactData["horizontal"].ToString());
-
-        //                 var textureData = File.ReadAllBytes(texturePath); // ERROR: The name 'File' does not exist in the current context?
-        //                 var texture = new Texture2D(2, 2);
-        //                 texture.LoadImage(textureData);
-
-        //                 visAsset.TextureArray = new Texture2D[] { texture };
-        //             }
-        //             else
-        //             {
-        //                 throw (new System.Exception());
-        //             }
-        //         }
-        //         catch (System.Exception e) { Debug.Log("VisAsset not suppported yet: " + type + " (" + jsonData["uuid"].ToString() + ")"); }
-        //     }
-
-
-        //     if (type == "texture")
-        //     {
-        //         SurfaceTextureVisAsset visAsset = ABRManager.CreateNode<SurfaceTextureVisAsset>(new System.Guid(jsonData["uuid"].ToString()));
-        //         visAsset.ImportTime = DateTime.Now;
-
-        //         ABRManager.Instance.SetNodeLabel(visAsset, jsonData["name"]?.ToString() ?? "");
-
-        //         var artifactData = jsonData["artifactData"];
-        //         try
-        //         {
-        //             if (artifactData.ContainsKey("image"))
-        //             {
-        //                 var texturePath = ArtifactDataPath(filePath, artifactData["image"].ToString());
-
-        //                 var textureData = File.ReadAllBytes(texturePath); // ERROR: The name 'File' does not exist in the current context?
-        //                 var texture = new Texture2D(2, 2);
-        //                 texture.LoadImage(textureData);
-
-        //                 visAsset.TextureArray = new Texture2D[] { texture };
-
-
-        //                 var normalPath = ArtifactDataPath(filePath, artifactData["normalmap"].ToString());
-
-        //                 var normalData = File.ReadAllBytes(normalPath); // ERROR: The name 'File' does not exist in the current context?
-        //                 var normal = new Texture2D(2, 2);
-        //                 normal.LoadImage(normalData);
-
-        //                 visAsset.NormalMapArray = new Texture2D[] { normal };
-        //             }
-        //             else
-        //             {
-        //                 throw (new System.Exception());
-        //             }
-        //         }
-        //         catch (System.Exception e) { Debug.Log("VisAsset not suppported yet: " + jsonData["name"]?.ToString() ?? "Untitled" + " (" + jsonData["uuid"].ToString() + ")"); }
-        //     }
+            catch (System.Exception e)
+            {
+            }
         }
     }
 }
