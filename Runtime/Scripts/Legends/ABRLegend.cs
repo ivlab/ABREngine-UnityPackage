b05e76ef7a46c7f5bf7f6ac1f44d6c800f0bbcc1
Bridger Herman <herma582@umn.edu>
2021-12-17 14:42:55 -0600

    Add glyph construction to ABRLegends

--- Runtime/Scripts/Legends/ABRLegend.cs
+++ Runtime/Scripts/Legends/ABRLegend.cs
@@ -17,6 +17,11 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
 namespace IVLab.ABREngine.Legends
 {
     /// <summary>
@@ -25,5 +30,39 @@ namespace IVLab.ABREngine.Legends
     /// </summary>
     public class ABRLegend
     {
+        public static SimpleGlyphDataImpression CreateGlyphLegendEntry(IColormapVisAsset colormap, IGlyphVisAsset glyph)
+        {
+            string glyphDataPath = "ABR/Legends/KeyData/Glyphs";
+            int numVars = 0;
+            if (colormap != null) numVars++;
+            if (glyph != null) numVars++;
+
+            RawDataset rds;
+            if (!ABREngine.Instance.Data.TryGetRawDataset(glyphDataPath, out rds))
+            {
+                rds = ABRLegendGeometry.Glyphs(numVars);
+                ABREngine.Instance.Data.ImportRawDataset(glyphDataPath, rds);
+            }
+
+            IKeyData glyphKeyData = null;
+            Dataset ds = null;
+            ABREngine.Instance.Data.TryGetDataset(DataPath.GetDatasetPath(glyphDataPath), out ds);
+            ds.TryGetKeyData(glyphDataPath, out glyphKeyData);
+
+            SimpleGlyphDataImpression gi = new SimpleGlyphDataImpression();
+            // Apply the artist-selected VisAssets
+            gi.colormap = colormap;
+            gi.glyph = glyph;
+
+            // Apply legend-specific entries
+            gi.keyData = glyphKeyData as PointKeyData;
+            gi.colorVariable = ds.GetAllScalarVars().First(v => v.Key.Contains("XAxis")).Value;
+            gi.glyphVariable = ds.GetAllScalarVars().First(v => v.Key.Contains("ZAxis")).Value;
+            gi.forwardVariable = ds.GetAllVectorVars().First(v => v.Key.Contains("Forward")).Value;
+            gi.upVariable = ds.GetAllVectorVars().First(v => v.Key.Contains("Up")).Value;
+            gi.glyphSize = new LengthPrimitive("0.3m");
+
+            return gi;
+        }
     }
 }
